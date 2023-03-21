a4bdf6c2b3cf57ccf3724caaaf1d82343a8b8c40
Asher <ash@coder.com>
2023-03-21 11:10:58 -0800

    Fix proxy error on web sockets
    Fixes #6088.

--- src/node/proxy.ts
+++ src/node/proxy.ts
@@ -3,9 +3,13 @@ import { HttpCode } from "../common/http"
 
 export const proxy = proxyServer.createProxyServer({})
 
+// The error handler catches when the proxy fails to connect (for example when
+// there is nothing running on the target port).
 proxy.on("error", (error, _, res) => {
-  res.writeHead(HttpCode.ServerError)
-  res.end(error.message)
+  // This could be for either a web socket or a regular request.  Despite what
+  // the types say, writeHead() will not exist on web socket requests (nor will
+  // status() from Express) so write out the code manually.
+  res.end(`HTTP/1.1 ${HttpCode.ServerError} ${error.message}\r\n\r\n`)
 })
 
 // Intercept the response to rewrite absolute redirects against the base path.
